ec6d940770395d1567db84ea3fc811aff973ffea
Anaelis <anaelisisabel@gmail.com>
2016-11-11 13:04:50 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+Template.topBar.events({
+	"click a#register":function(event,template){
+		event.preventDefault();
+		Modal.show('register');
+	},
+	"click a#login":function(event,template){
+		event.preventDefault();
+		Modal.show('login');
+	},
+
+	"click a#logout":function(event,template){
+		event.preventDefault();
+		Modal.show('logout');
+	},
+});
